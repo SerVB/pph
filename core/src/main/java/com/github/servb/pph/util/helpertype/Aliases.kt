0c9634830347e68f7752cf3c1c22e290a5c7fa2e
SerVB <servbul@yandex.ru>
2018-07-02 23:54:30 +0300

    Move gxl.application

---
+++ core/src/main/java/com/github/servb/pph/util/helpertype/Aliases.kt
@@ -0,0 +1,7 @@
+package com.github.servb.pph.util.helpertype
+
+typealias DWORD = Int
+typealias HINSTANCE = Int
+typealias HWND = Int
+typealias LPCWSTR = String
+typealias VOID_STAR = Nothing
---
+++ core/src/main/java/com/github/servb/pph/util/helpertype/Defines.kt
@@ -0,0 +1,5 @@
+package com.github.servb.pph.util.helpertype
+
+const val HPC_JORNADA = false
+const val OS_WINCE = false
+const val OS_WIN32 = true
