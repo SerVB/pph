ccab0edad83e40c4fdf75654acc9ffd666718eae
SerVB <servbul@yandex.ru>
2019-04-06 17:07:00 +0300

    Add infix operator `or` for concise code

--- core/src/main/kotlin/com/github/servb/pph/util/helpertype/EnumTestingInterfaces.kt
+++ core/src/main/kotlin/com/github/servb/pph/util/helpertype/EnumTestingInterfaces.kt
@@ -1,19 +1,20 @@
 package com.github.servb.pph.util.helpertype
 
-/** [v] has to be unique. */
-interface UniqueValueEnum {
+interface EnumWithValue {
     val v: Int
 }
 
+/** [v] has to be unique. */
+interface UniqueValueEnum : EnumWithValue
+
 /** Need to have an element containing "COUNT" and its value has to be a number of previous members. */
-interface CountValueEnum {
-    val v: Int
-}
+interface CountValueEnum : EnumWithValue
 
 /**
  * Need to have an element containing "COUNT" and its value has to be a number of previous members except "UNDEFINED"
  * member (if there is).
  */
-interface UndefinedCountValueEnum {
-    val v: Int
-}
+interface UndefinedCountValueEnum : EnumWithValue
+
+infix fun EnumWithValue.or(other: EnumWithValue): Int = this.v or other.v
+infix fun Int.or(other: EnumWithValue): Int = this or other.v
