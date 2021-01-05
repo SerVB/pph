76b3099405d59f0f0433801477196acecf5f9c87
SerVB <servbul@yandex.ru>
2021-01-05 13:35:59 +0300

    Port logo drawing algorithm

--- src/commonMain/kotlin/com/github/servb/pph/pheroes/Game/Credits.kt
+++ src/commonMain/kotlin/com/github/servb/pph/pheroes/Game/Credits.kt
@@ -1,27 +1,59 @@
 package com.github.servb.pph.pheroes.Game
 
+import com.github.servb.pph.gxlib.IDibPixelIPointer
 import com.github.servb.pph.gxlib.IiDib
 import com.github.servb.pph.gxlib.copyTo
 import com.github.servb.pph.gxlib.iDib
 import com.github.servb.pph.util.ISizeInt
 import com.github.servb.pph.util.SizeT
 import com.soywiz.korim.bitmap.Bitmap32
-import com.soywiz.korim.bitmap.context2d
+import com.soywiz.korim.color.RGBA
+import com.soywiz.korim.color.convertTo
 import com.soywiz.korim.format.readBitmap
 import com.soywiz.korio.file.std.resourcesVfs
 import com.soywiz.korma.geom.IPointInt
 import com.soywiz.korma.geom.IRectangleInt
 import com.soywiz.korma.geom.PointInt
-import com.soywiz.korma.geom.SizeInt
 import kotlin.properties.Delegates
+
+private const val R16MASK: UInt = 0xF800u
+private const val G16MASK: UInt = 0x07E0u
+private const val B16MASK: UInt = 0x001Fu
+
+private val RB16MASK: UInt = R16MASK or B16MASK
+private const val RB16ROUND: UInt = 0x8010u
+private const val G16ROUND: UInt = 0u  // 0x0400u  // commented in sources
+
+private fun blend16fast(p1: UInt, p2: UInt, a: UInt): UInt {
+    val rbdst = p2 and RB16MASK
+    var res = (rbdst + ((((p1 and RB16MASK) - rbdst) * a + RB16ROUND) shr 5)) and RB16MASK
+    val gdst = p2 and G16MASK
+    res = res or ((gdst + ((((p1 and G16MASK) - gdst) * a + G16ROUND) shr 5)) and G16MASK)
+    return res
+}
+
+private fun blit_16aline(src: Bitmap32, srcOffset: Int, dst: IDibPixelIPointer, npix: SizeT): Int {
+    var srcOffset = srcOffset
+    val pdst = dst.copy()
+    repeat(npix) {
+        val srcColor = src.data[srcOffset]
+        val srcAlpha = (srcColor.a.toUInt() shr 3) and 0x1Fu
+        val srcRGB565 = RGBA.convertTo(srcColor.value, IiDib.Type.RGB.colorFormat16)
+
+        pdst[0] = blend16fast(srcRGB565.toUInt(), pdst[0].toUInt(), srcAlpha).toUShort()
+
+        ++srcOffset
+        pdst.incrementOffset(1)
+    }
+    return srcOffset
+}
 
 // InitMotionBlur, InitBump, InitLight and related are deleted as unused in sources
 class iCreditsComposer {
 
     private val m_back: iDib = iDib()
     private var m_pos: SizeT by Delegates.notNull()
-    private val m_logo: iDib = iDib()
-    private val buffer = Bitmap32(320, 240)  // todo: port blitting and get rid of this buffer
+    private lateinit var m_logo: Bitmap32
 
     private var m_bEnd: Boolean = false
     private var m_scrPos: Int = 0
@@ -37,9 +69,7 @@ class iCreditsComposer {
         m_back.Init(ISizeInt(320, 720), IiDib.Type.RGB)
         resourcesVfs["pheroes/bin/Resources/hmm/GFX/Pix/MenuBack.png"].readBitmap().copyTo(m_back)
 
-        val title = resourcesVfs["pheroes/bin/Resources/hmm/GFX/Pix/title_hmm.png"].readBitmap()
-        m_logo.Init(SizeInt(title.size), IiDib.Type.RGBA)
-        title.copyTo(m_logo)
+        m_logo = resourcesVfs["pheroes/bin/Resources/hmm/GFX/Pix/title_hmm.png"].readBitmap().toBMP32()
 
         m_pos = 0
         t = 0.0
@@ -69,21 +99,15 @@ class iCreditsComposer {
             m_bEnd
         }
 
-        // todo: try to implement original blitting:
-//        // compose 'Heroes' logo directly on surface  // todo: won't work on other resolutions
-//        val src = m_logo.GetPtr()
-//        val dst = surface.GetPtr()
-//        // magic position constant ;)
-//        dst.incrementOffset(320 * 4 + 45)
-//        repeat(m_logo.GetHeight()) { nh ->
-//            src.incrementOffset(blit_16aline(src, dst, m_logo.GetWidth() / 2))
-//            dst.incrementOffset(320)
-//        }
-        surface.backingBitmap.copy(0, 0, buffer, 0, 0, buffer.width, buffer.height)
-        buffer.context2d {
-            drawImage(m_logo.backingBitmap, 45, 4)  // magic position constant ;)
+        // compose 'Heroes' logo directly on surface  // todo: won't work on other resolutions
+        var srcOffset = 0
+        val dst = surface.GetPtr()
+        // magic position constant ;)
+        dst.incrementOffset(320 * 4 + 45)
+        repeat(m_logo.height) {
+            srcOffset = blit_16aline(m_logo, srcOffset, dst, m_logo.width)
+            dst.incrementOffset(320)
         }
-        buffer.copy(0, 0, surface.backingBitmap, 0, 0, buffer.width, buffer.height)
     }
 
     fun StartCredits() {
