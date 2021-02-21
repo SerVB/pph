7a8a401ca6661e080a59666dfbb1eef9eb6b0778
SerVB <servbul@yandex.ru>
2021-02-21 16:20:27 +0300

    Remove unused dependency

--- map-editor/build.gradle.kts
+++ map-editor/build.gradle.kts
@@ -6,8 +6,6 @@ korge {
     jvmMainClassName = "com.github.servb.pph.pheroes.mapEditor.ExportDlgKt"
 
     targetJvm()
-
-    bundle("https://github.com/korlibs/korlibs-bundle-source-extensions.git::korma-rectangle-experimental-ext::696a97640bb93a66f07ca008cca84b1ae4013e57##d2d9e3eb8f9f8eb5c137e847677eb8b3e9038c30d1f4457d1bd05cafc5c3f251")
 }
 
 kotlin {
